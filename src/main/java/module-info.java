bbfbb6cfc1e4f4bac61863d75fa9f45bdd199194
Dakotady <67987621+Dakotady@users.noreply.github.com>
2023-05-06 18:32:23 -0500

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module task1.software1_c482_qkm2_task1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens task1.software1_c482_qkm2_task1 to javafx.fxml;
+    exports task1.software1_c482_qkm2_task1;
+}
---
+++ src/main/java/task1/software1_c482_qkm2_task1/MainController.java
@@ -0,0 +1,14 @@
+package task1.software1_c482_qkm2_task1;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class MainController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
