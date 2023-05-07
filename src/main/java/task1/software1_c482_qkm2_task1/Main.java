4f84b3d0b2917672cc6f29ad1efdb2512e5a74fa
Dakotady <67987621+Dakotady@users.noreply.github.com>
2023-05-06 23:43:04 -0500

    added GUI and functional exit button

--- src/main/java/task1/software1_c482_qkm2_task1/Main.java
+++ src/main/java/task1/software1_c482_qkm2_task1/Main.java
@@ -7,11 +7,22 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/**
+ * This is the main class that opens when the application starts *
+ *<p>
+ * <b>FUTURE ENHANCEMENT:</b> I would tie this to a SQL database so the information could be sent shared better.<br>
+ * I would also combine the Add part and add product form and use another radio button to choose what is being added.
+ *</p>
+ * <p>
+ * <b>RUNTIME ERROR:</b> I ran into an issue at the start of this project and couldn't get<br>
+ * the close button to work and realized I misspelled the object.
+ *</p>
+ */
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("MainForm.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 400);
         stage.setTitle("");
         stage.setScene(scene);
         stage.show();
--- src/main/java/task1/software1_c482_qkm2_task1/MainController.java
+++ src/main/java/task1/software1_c482_qkm2_task1/MainController.java
@@ -1,14 +1,27 @@
 package task1.software1_c482_qkm2_task1;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
-public class MainController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
     @FXML
-    private Label welcomeText;
+
+    private Button exitApp;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onExitappButtonClick() {
+        Platform.exit();
+        }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.println("I am initialized");
     }
 }
